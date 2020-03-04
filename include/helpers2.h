6299f009656415094fcaeff457fb559acc584b45
lsy-it-1995 <61727133+lsy-it-1995@users.noreply.github.com>
2020-03-03 17:52:51 -0800

    Add files via upload

---
+++ include/helpers2.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "hw2.h"
+
+void createKey(char*, char* );
+char morseToKey(char*, char* );
+int toMorse(FILE*, char** );
+int FMCEncrypt(FILE *, char* , FILE* );
+int fromMorse(char* , FILE* );
+int FMCDecrypt(FILE*, char* , FILE* );
+
